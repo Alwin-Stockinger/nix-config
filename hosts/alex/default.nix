741c846e795c79924800bf0ee45c358c0b37dabd
Alwin <alwin@stockinger.tech>
2024-03-03 19:22:38 +0100

    add docker to alex

--- hosts/alex/default.nix
+++ hosts/alex/default.nix
@@ -73,12 +73,24 @@ in {
       enable = true;
 
       # Create a `docker` alias for podman, to use it as a drop-in replacement
-      dockerCompat = true;
+      #dockerCompat = true;
 
       # Required for containers under podman-compose to be able to talk to each other.
       defaultNetwork.settings.dns_enabled = true;
     };
+
+    docker = {
+      enable = true;
+      rootless = {
+        enable = true;
+        setSocketVariable = true;
+      };
+      daemon.settings = {
+        data-root = "/data/docker";
+      };
+    };
   };
+  users.users.alwin.extraGroups = ["docker"];
 
   hardware.enableRedistributableFirmware = true;
   system.stateVersion = "23.11";
