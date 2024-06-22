e67a15f276bdc556a3b5f40a2c5624e728d35789
Alwin Stockinger <alwin@stockinger.tech>
2024-06-22 12:49:24 +0200

    things

--- nixos/alex/default.nix
+++ nixos/alex/default.nix
@@ -79,7 +79,7 @@
     acceptTerms = true;
     defaults = {
       email = "alwin@stockinger.tech";
-      dnsPropagationCheck = true;
+      dnsPropagationCheck = false;
       dnsProvider = "cloudflare";
       server = "https://acme-staging-v02.api.letsencrypt.org/directory";
       credentialFiles = {
