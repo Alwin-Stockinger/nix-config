2f8c1dbc1548a6077443ea89c18859b5a22598d2
Alwin Stockinger <alwin@stockinger.tech>
2024-06-22 13:00:42 +0200

    fixed it, how? not sure

--- nixos/alex/default.nix
+++ nixos/alex/default.nix
@@ -79,9 +79,9 @@
     acceptTerms = true;
     defaults = {
       email = "alwin@stockinger.tech";
-      dnsPropagationCheck = false;
+      dnsPropagationCheck = true;
       dnsProvider = "cloudflare";
-      server = "https://acme-staging-v02.api.letsencrypt.org/directory";
+#      server = "https://acme-staging-v02.api.letsencrypt.org/directory";
       credentialFiles = {
         "CLOUDFLARE_EMAIL_FILE" = config.sops.secrets.cloudflare_email.path;
         "CLOUDFLARE_API_KEY_FILE" = config.sops.secrets.cloudflare_token.path;
