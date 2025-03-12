eb5d38ebf4baf9f2c595de7b90df6c8c8e8f7eb5
Alwin <alwin@stockinger.tech>
2025-03-12 18:35:30 +0100

    in the end apple always wins

--- home/alwin/features/desktop.nix
+++ home/alwin/features/desktop.nix
@@ -171,6 +171,8 @@
                     }
 
                     sensitivity = 0 # -1.0 - 1.0, 0 means no modification.
+
+                    natural_scroll = true
                 }
 
                 general {
